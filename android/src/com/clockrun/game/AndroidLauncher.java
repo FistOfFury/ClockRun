7d3a8b51a9d134cd88b9693d8cedc038fb4c518b
dana_cohen <cohedana@gmail.com>
2020-04-17 13:47:42 +0300

    open project

---
+++ android/src/com/clockrun/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.clockrun.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.clockrun.game.main_game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new main_game(), config);
+	}
+}
---
+++ desktop/src/com/clockrun/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.clockrun.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.clockrun.game.main_game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new main_game(), config);
+	}
+}
