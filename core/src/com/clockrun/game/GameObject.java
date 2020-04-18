f4886b47097ee7a0891ea04c124b7388ffca6b05
ShiriCohen <shirurka@gmail.com>
2020-04-18 23:36:50 +0300

    player and gameobject

--- core/src/com/clockrun/game/GameObject.java
+++ core/src/com/clockrun/game/GameObject.java
@@ -1,16 +1,41 @@
 package com.clockrun.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionShape;
 import com.badlogic.gdx.utils.Disposable;
 
 public class GameObject extends ModelInstance implements Disposable {
 
     private Model model;
-    public GameObject(Model model, String node) {
+    private int cur_road;
+    static int NUM_ROADS = 3;
+
+    public GameObject(Model model, String node, int cur_road) {
         super(model, node);
         this.model = model;
+        this.cur_road = cur_road;
+    }
+
+    public void move_left()
+    {
+        this.cur_road = (this.cur_road - 1 ) % GameObject.NUM_ROADS;
+        this.update_Road();
+    }
+
+    public void move_right()
+    {
+        this.cur_road = (this.cur_road + 1 ) % NUM_ROADS;
+        this.update_Road();
+    }
+
+    private void update_Road()
+    {
+        int x_place = (int)((Gdx.graphics.getWidth() * (NUM_ROADS + 0.5)) / NUM_ROADS );
+        Vector3 cur_place = this.transform.getTranslation(new Vector3());
+        this.transform.setTranslation(new Vector3(x_place,cur_place.y,cur_place.y));
     }
 
 
--- core/src/com/clockrun/game/Player.java
+++ core/src/com/clockrun/game/Player.java
@@ -1,9 +1,34 @@
 package com.clockrun.game;
 
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Vector3;
+
 public class Player extends GameObject {
+    int lives;
+    int score;
+
     public Player()
     {
-        Model.
-        super()
+        super(new ModelBuilder().createBox(5f, 5f, 5f,
+                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+                Usage.Position | Usage.Normal), "player", GameObject.NUM_ROADS / 2);
+        this.score = 0;
+        this.lives = 3;
     }
+
 }
